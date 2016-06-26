dc3c435351af57165e4f30df13c26d059a239a9e
Eugene Brodsky <fupslot@gmail.com>
2016-06-26 18:24:51 +0300

    Convert tabs to spaces

--- index.js
+++ index.js
@@ -9,67 +9,67 @@ const git = require('./lib/lastcommit');
 const config = require('./package.json');
 
 module.exports = function (opts) {
-	opts = opts || {};
+  opts = opts || {};
 
-	let logger;
+  let logger;
 
   if (opts.analyticss) {
-  	logger = require('./lib/AnalyticssIOLogger')(opts);
-  	gutil.log(config.name, 'use analyticss.io');
+    logger = require('./lib/AnalyticssIOLogger')(opts);
+    gutil.log(config.name, 'use analyticss.io');
   } else if (opts.aws) {
-  	logger = require('./lib/S3Logger')(opts);
-  	gutil.log(config.name, 'use Amazon S3');
+    logger = require('./lib/S3Logger')(opts);
+    gutil.log(config.name, 'use Amazon S3');
   } else {
-  	gutil.log(config.name, gutil.colors.red(`wrong configuration file`));
-  	gutil.log(config.name, gutil.colors.red(`see ${config.homepage} for more details`));
-  	process.exit(1);
+    gutil.log(config.name, gutil.colors.red(`wrong configuration file`));
+    gutil.log(config.name, gutil.colors.red(`see ${config.homepage} for more details`));
+    process.exit(1);
   }
 
 
-	// TODO:
+  // TODO:
   // let stream = new Stream(logger);
 
-	var stream = new Transform({ objectMode: true });
+  var stream = new Transform({ objectMode: true });
 
-	stream._transform = (file, encoding, next) => {
-		if (file.isNull()) {
-			cb(null, file);
-			return;
-		}
+  stream._transform = (file, encoding, next) => {
+    if (file.isNull()) {
+      cb(null, file);
+      return;
+    }
 
-		if (file.isStream()) {
-			cb(new gutil.PluginError(config.name, 'Streaming not supported'));
-			return;
-		}
+    if (file.isStream()) {
+      cb(new gutil.PluginError(config.name, 'Streaming not supported'));
+      return;
+    }
 
-		try {
-			git.getLastCommit((err, commit) => {
+    try {
+      git.getLastCommit((err, commit) => {
 
-				if (err || typeof commit === 'string') {
-					commit = null;
-					let message = `Unable to collect a commit data, ${(err || commit)}`;
-					gutil.log(config.name, gutil.colors.red(message));
-				}
+        if (err || typeof commit === 'string') {
+          commit = null;
+          let message = `Unable to collect a commit data, ${(err || commit)}`;
+          gutil.log(config.name, gutil.colors.red(message));
+        }
 
-				new analyzer(file.contents.toString(), opts, (err, data) => {
-					if (err) {
-						cb(new gutil.PluginError(config.name + ' (error)', err));
-						return;
-					}
+        new analyzer(file.contents.toString(), opts, (err, data) => {
+          if (err) {
+            cb(new gutil.PluginError(config.name + ' (error)', err));
+            return;
+          }
 
-					data.created = Date.now();
-					data.commit = commit;
+          data.created = Date.now();
+          data.commit = commit;
 
-					logger.send(data, file, (err) => {
-					  next(null, file);
-					});
+          logger.send(data, file, (err) => {
+            next(null, file);
+          });
 
-				});
-			});
-		} catch (ex) {
-			cb(new gutil.PluginError(config.name+' (error)', ex));
-		}
-	};
+        });
+      });
+    } catch (ex) {
+      cb(new gutil.PluginError(config.name+' (error)', ex));
+    }
+  };
 
-	return stream;
+  return stream;
 };
